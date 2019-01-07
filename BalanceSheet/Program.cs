fceb1ca6057b7abf8c5de02d4998a1bafa81fc03
David <davidmrose@verizon.net>
2019-01-07 15:48:14 -0500

    Initial program.

--- BalanceSheet/Program.cs
+++ BalanceSheet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,21 @@ namespace BalanceSheet
 	{
 		static void Main(string[] args)
 		{
+			double total = 0.0;
+			var list = new List<double>() { 30.0, -30.0, 40.0, 50.0, -10.0, 20.0 }
+			.ToObservable();
+
+			list.Delay(TimeSpan.FromSeconds(2))
+				.Subscribe(
+				val =>
+				{
+					total += val;
+					string cred = val < 0 ? "debit" : "credit";
+					string msg = $"${val} {cred} Balance = ${total}";
+					Console.WriteLine(msg);
+				});
+
+			Console.ReadKey();
 		}
 	}
 }
